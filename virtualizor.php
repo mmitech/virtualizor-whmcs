0d83cf31f7df43294af9a8c938621c1b921426e0
mmitech <dzsi2007@gmail.com>
2024-01-10 08:40:10 +0100

    resolve the hacking attemp message

--- virtualizor.php
+++ virtualizor.php
@@ -3038,7 +3038,8 @@ function virtualizor_enduser_panel($vars){
 	    	$ser_data = (array) $query[0];
 	    	//print_r($res);
 	
-	        $tmp_hostname = $ser_data['hostname'];
+	        // $tmp_hostname = $ser_data['hostname'];
+			$tmp_hostname = "dashboard.mmitech.info"
 	        if(empty($tmp_hostname)){
 	        	$tmp_hostname = $ser_data['ipaddress'];
 	        }
@@ -3098,16 +3099,16 @@ function virtualizor_primarySidebar($primarySidebar){
 		
 	//@var \WHMCS\View\Menu\Item $primarySidebar
 	$newMenu = $primarySidebar->addChild(
-		'Virtualizor',
+		'Dashboard',
 		array(
-			'name' => 'Virtualizor',
-			'label' => 'Virtualizor',
+			'name' => 'Dashboard',
+			'label' => 'Dashboard',
 			'order' => 99,
 			'icon' => 'fa-cubes',
 		)
 	);
 	$newMenu->addChild(
-		'Virtualizor',
+		'Dashboard',
 		array(
 			'name' => 'Enduser Panel',
 			'label' => 'Enduser Panel',
